56961bc5ee3903832fc9405bda06d3a25077266d
PosticaDenis <postica.denis.95@gmail.com>
2017-01-16 23:00:02 +0200

    adding ES labs

---
+++ FAF-141/Postica_Denis/ES-projects/lab3/button.c
@@ -0,0 +1,18 @@
+#include "button.h"
+
+
+void initButtonOne() {
+	DDRC &= ~(1 << PORTC0) ;
+}
+
+void initButtonTwo() {
+	DDRC &= ~(1 << PORTC1) ;
+}
+
+int isButtonOnePressed() {
+	return PINC & (1<<PORTC0);
+}
+
+int isButtonTwoPressed() {
+	return PINC & (1<<PORTC1);
+}
